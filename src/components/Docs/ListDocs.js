a0f7677f8fc8360aa8b1e09b7ff83caedf83f3cb
Uson uulu Mairambek <usonuulu.mairambek.pi2@gmail.com>
2021-04-28 18:07:10 +0600

    дизйн

--- src/components/Docs/ListDocs.js
+++ src/components/Docs/ListDocs.js
@@ -1,40 +1,43 @@
-import React from "react"
-import { CardMedia, Typography } from "@material-ui/core"
-import Grid from "@material-ui/core/Grid"
-import "./index.css"
-import ExcelIcon from "../../redux/uils/icons/iconfinder_excel_272697.png"
+import React from "react";
+import { CardMedia, Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import "./index.css";
+import ExcelIcon from "./img/excel.svg";
 
 export default function ListDocs(props) {
-  const docsList = [{name: "1", file: props.data.excel}, 
-                    {name: "2", file: props.data.excel},
-                    {name: "3", file: props.data.excel},
-                    {name: "4", file: props.data.excel},]
+  const docsList = [
+    { name: "1", file: props.data.excel },
+    { name: "2", file: props.data.excel },
+    { name: "3", file: props.data.excel },
+    { name: "4", file: props.data.excel },
+  ];
   return (
     <div className="container">
       <Typography variant="h5" component="h6">
         {props.data.template}
       </Typography>
-
       <Grid container direction="row" justify="flex-start" alignItems="center">
         {docsList.map((excel, id) => {
           return (
             <div key={id} className="card-file">
-                <div className="card-header">{excel.name}</div>
-                    <CardMedia
-                      className="icon-card"
-                      type="file"
-                      image={ExcelIcon}
-                      // onChange={{ pathname: `/docs/${excel.id}`, state: { excel }}}
-                      />
-                    <h5 className="card-footer">
-                    <a href={excel.file} download>
-                        Скачать
-                    </a>
-                    </h5>
+              <div className="card-header">{excel.name}</div>
+              <CardMedia
+                className="icon-card"
+                type="file"
+                image={ExcelIcon}
+                // onChange={{ pathname: `/docs/${excel.id}`, state: { excel }}}
+              />
+              <h5 className="card-footer">
+                <div className="card-footer-downloader">
+                  <a href={excel.file} download>
+                    Скачать
+                  </a>
+                </div>
+              </h5>
             </div>
-          )
+          );
         })}
       </Grid>
     </div>
-  )
+  );
 }
