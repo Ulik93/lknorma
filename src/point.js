1b1a4f5211a4233053c8486e49a9222d9c072444
beksultan <beksultan.mamashov@gmail.com>
2021-05-03 13:23:38 +0600

    asd

--- src/point.js
+++ src/point.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-const point = "https://367db693358f.ngrok.io"
-=======
-
 const point = "http://185.29.184.52:3000"
->>>>>>> 381bf15620ed1ea41503e5daeb41093dfb1df93b
 
 export default point
 
